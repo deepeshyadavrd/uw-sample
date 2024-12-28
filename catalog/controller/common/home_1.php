37818f896d2c358a9a60b7cfd58b391c122a2fd0
deepeshyadavrd <raoshsbh6@gmail.com>
2024-12-28 15:01:43 +0530

    .

--- catalog/controller/common/home_1.php
+++ catalog/controller/common/home_1.php
@@ -1,12 +1,12 @@
 <?php
-class ControllerCommonHome extends Controller {
-	public function index() {
-		$this->document->setTitle($this->config->get('config_meta_title'));
-		$this->document->setDescription($this->config->get('config_meta_description'));
-		$this->document->setKeywords($this->config->get('config_meta_keyword'));
+class ControllerCommonHome extends Controller{
+	public function index(){
+		$this->document->setTitle($this->config->get('config_meta_title') );
+		$this->document->setDescription($this->config->get('config_meta_description') );
+		$this->document->setKeywords($this->config->get('config_meta_keyword') );
 		
 		if (isset($this->request->get['route'])) {
-			$this->document->addLink($this->config->get('config_url'), 'canonical');
+			$this->document->addLink($this->config->get('config_url'), 'canonical' );
 		}
 		if (isset($this->session->data['success'])) {
 			$data['success'] = $this->session->data['success'];
