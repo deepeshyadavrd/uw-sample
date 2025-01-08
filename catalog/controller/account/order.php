23505e0f2a78bcc2bd4e79e5cf8051b327d5703e
deepeshyadavrd <raoshsbh6@gmail.com>
2025-01-08 17:53:33 +0530

    .

--- catalog/controller/account/order.php
+++ catalog/controller/account/order.php
@@ -264,10 +264,10 @@ class ControllerAccountOrder extends Controller {
 
 		$data['continue'] = $this->url->link('account/account', '', true);
 
-		$data['column_left'] = $this->load->controller('common/column_left');
-		$data['column_right'] = $this->load->controller('common/column_right');
-		$data['content_top'] = $this->load->controller('common/content_top');
-		$data['content_bottom'] = $this->load->controller('common/content_bottom');
+		// $data['column_left'] = $this->load->controller('common/column_left');
+		// $data['column_right'] = $this->load->controller('common/column_right');
+		// $data['content_top'] = $this->load->controller('common/content_top');
+		// $data['content_bottom'] = $this->load->controller('common/content_bottom');
 		$data['footer'] = $this->load->controller('common/footer');
 		$data['header'] = $this->load->controller('common/header');
 		$data['menu'] = $this->load->controller('common/menu');
