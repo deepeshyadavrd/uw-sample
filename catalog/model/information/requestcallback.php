0caf732043b63858aad56dc13e638cad02e9e20e
deepeshyadavrd <raoshsbh6@gmail.com>
2025-01-14 17:57:08 +0530

    .

--- catalog/model/information/requestcallback.php
+++ catalog/model/information/requestcallback.php
@@ -4,6 +4,7 @@ class ModelInformationRequestcallback extends Model {
 		$this->db->query("INSERT INTO " . DB_PREFIX . "request_callback SET name = '" . $this->db->escape($data['name']) . "', email = '" . $this->db->escape($data['email']) . "', mobile = '" . $this->db->escape($data['mobile']) . "', message = '" . $this->db->escape($data['message']) . "', date_added = NOW()");
 
 		$rc_id = $this->db->getLastId();
+		
 		return $rc_id;
 	}
 }
