48ab1241b3d6ef435f28cc9c81b3269ff8cf7c85
deepeshyadavrd <raoshsbh6@gmail.com>
2025-02-05 16:14:02 +0530

    .

---
+++ catalog/controller/extension/feed/sitemaps.php
@@ -0,0 +1,45 @@
+<?php
+class ControllerExtensionFeedSitemaps extends Controller {
+    public function index() {
+        if ($this->config->get('feed_google_sitemap_status')) {
+            $output = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
+            $output .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+            
+            $baseUrl = $this->config->get('config_url');
+
+            // Product Sitemap
+            $output .= "    <sitemap>\n";
+            $output .= "        <loc>{$baseUrl}sitemap-products.xml</loc>\n";
+            $output .= "        <lastmod>" . date('Y-m-d') . "</lastmod>\n";
+            $output .= "    </sitemap>\n";
+
+            // Category Sitemap
+            $output .= "    <sitemap>\n";
+            $output .= "        <loc>{$baseUrl}sitemap-categories.xml</loc>\n";
+            $output .= "        <lastmod>" . date('Y-m-d') . "</lastmod>\n";
+            $output .= "    </sitemap>\n";
+
+            // Information Pages Sitemap
+            $output .= "    <sitemap>\n";
+            $output .= "        <loc>{$baseUrl}sitemap-info.xml</loc>\n";
+            $output .= "        <lastmod>" . date('Y-m-d') . "</lastmod>\n";
+            $output .= "    </sitemap>\n";
+
+            $output .= '</sitemapindex>';
+
+            $this->response->addHeader('Content-Type: application/xml');
+            $this->response->setOutput($output);
+        }
+    }
+
+    public function generate() {
+        $this->load->model('extension/feed/sitemap');
+
+        // Generate different sitemap sections
+        $this->model_extension_feed_sitemap->generateProductsSitemap();
+        $this->model_extension_feed_sitemap->generateCategoriesSitemap();
+        $this->model_extension_feed_sitemap->generateInfoPagesSitemap();
+
+        echo "✅ Sitemaps Generated Successfully!";
+    }
+}
