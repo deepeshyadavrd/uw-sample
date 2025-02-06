7a27000649c61819235df5558d8a4bfa0e1b5e8a
deepeshyadavrd <raoshsbh6@gmail.com>
2025-02-06 15:55:19 +0530

    .

---
+++ admin/language/en-gb/design/home_section.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']     = 'Homepage Section';
+
+// Text
+$_['text_success']      = 'Success: You have modified homepage section!';
+$_['text_list']         = 'Homepage Section List';
+$_['text_add']          = 'Add Homepage Section';
+$_['text_edit']         = 'Edit Homepage Section';
+$_['text_default']      = 'Default';
+
+// Column
+$_['column_name']       = 'Homepage Section Name';
+$_['column_status']     = 'Status';
+$_['column_action']     = 'Action';
+
+// Entry
+$_['entry_name']        = 'Homepage Section Name';
+$_['entry_title']       = 'Title';
+$_['entry_link']        = 'Link';
+$_['entry_image']       = 'Image';
+$_['entry_status']      = 'Status';
+$_['entry_sort_order']  = 'Sort Order';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify Homepage Sections!';
+$_['error_name']       = 'Homepage Section Name must be between 3 and 64 characters!';
+$_['error_title']      = 'Homepage Section Title must be between 2 and 64 characters!';
