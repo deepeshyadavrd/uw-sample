39947a56c9241263c0dcb12393970ff99c4e6e16
deepeshyadavrd <raoshsbh6@gmail.com>
2025-02-20 17:21:02 +0530

    new function for getting product details

--- catalog/controller/product/new_products.php
+++ catalog/controller/product/new_products.php
@@ -40,6 +40,11 @@ class ControllerProductNewProducts extends Controller {
             } else {
                 $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
             }
+            if ($result['image2']) {
+                $image2 = $this->model_tool_image->resize($result['image2'], 400, 281);
+            } else {
+                $image2 = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+            }
             
             if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
                 $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
@@ -81,6 +86,7 @@ class ControllerProductNewProducts extends Controller {
             $data['products'][] = array(
                 'product_id'  => $result['product_id'],
                 'thumb'       => $image,
+                'thumb2'    => $image2,
                 'name'        => $name,
                 'finish'        => $finish,
                 'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
