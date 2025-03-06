cbabb2b8cfc329b52a9fc99c84fe706fe8b43dd9
deepeshyadavrd <raoshsbh6@gmail.com>
2025-03-06 15:42:57 +0530

    .

--- catalog/controller/common/header.php
+++ catalog/controller/common/header.php
@@ -83,12 +83,13 @@ class ControllerCommonHeader extends Controller {
 		
 		$req_uri = end(explode('/',$_SERVER[REQUEST_URI]));
 			$req_uri = strtok($req_uri, '?');
-
+			$req_uri = strtok($req_uri, '&');
+print_r($req_uri);
 		$data['canonical'] = '';
 		if($data['title']!='The page you requested cannot be found!'){
 			$data['canonical'] = $server.strtolower($req_uri);
 		}
-		
+
 		$data['language'] = $this->load->controller('common/language');
 		$data['currency'] = $this->load->controller('common/currency');
 		$data['search'] = $this->load->controller('common/search');
