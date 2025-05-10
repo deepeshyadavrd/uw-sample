e6045a28d690039f430acffb0f2f8797ddf79e25
deepeshyadavrd <raoshsbh6@gmail.com>
2025-05-10 13:37:59 +0530

    .

--- catalog/controller/checkout/cart_custom.php
+++ catalog/controller/checkout/cart_custom.php
@@ -7,18 +7,43 @@ class ControllerCheckoutCartCustom extends Controller {
         $this->load->model('catalog/product');
 
         $this->document->setTitle($this->language->get('heading_title'));
-
+$this->load->model('tool/image');
+			$this->load->model('tool/upload');
         $data['products'] = array();
+        $products = $this->cart->getProducts();
 
-        foreach ($this->cart->getProducts() as $product) {
+        
+        foreach ($products as $product) {
+            if ($product['image']) {
+				$image = $this->model_tool_image->resize($product['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_cart_height'));
+			} else {
+				$image = '';
+			}
+            if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+				$unit_price = $this->tax->calculate($product['price'], $product['tax_class_id'], $this->config->get('config_tax'));
+				
+				$price = $this->currency->format($unit_price * $product['quantity'], $this->session->data['currency']);
+				$total = $this->currency->format($product['mrp'] * $product['quantity'], $this->session->data['currency']);
+			} else {
+				$price = false;
+				$total = false;
+			}
+            $percentage_off = round((($product['mrp'] - $product['price']) / $product['mrp'])* 100);
             $data['products'][] = array(
-                'key'      => $product['key'],
-                'name'     => $product['name'],
-                'model'    => $product['model'],
-                'quantity' => $product['quantity'],
-                'price'    => $this->currency->format($product['price'], $this->session->data['currency']),
-                'total'    => $this->currency->format($product['total'], $this->session->data['currency']),
-                'href'     => $this->url->link('product/product', 'product_id=' . $product['product_id'])
+                'cart_id'   => $product['cart_id'],
+					'product_id'   => $product['product_id'],
+					'thumb'     => $image,
+					'name'      => $product['name'],
+					'model'     => $product['model'],
+					// 'option'    => $option_data,
+					// 'recurring' => $recurring,
+					'quantity'  => $product['quantity'],
+					'stock'     => $product['stock'] ? true : !(!$this->config->get('config_stock_checkout') || $this->config->get('config_stock_warning')),
+					'reward'    => ($product['reward'] ? sprintf($this->language->get('text_points'), $product['reward']) : ''),
+					'price'     => $price,
+					'total'     => $total,
+					'discount'	=> $percentage_off,
+					'href'      => $this->url->link('product/product', 'product_id=' . $product['product_id'])
             );
         }
 
