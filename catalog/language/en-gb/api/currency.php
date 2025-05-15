fbd327b570b8bac77b557c3c3b1e4f604e0b3d74
deepeshyadavrd <raoshsbh6@gmail.com>
2025-05-15 10:36:36 +0530

    .

--- catalog/language/en-gb/api/currency.php
+++ catalog/language/en-gb/api/currency.php
@@ -3,5 +3,5 @@
 $_['text_success']     = 'Success: Your currency has been changed!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to access the API!';
+$_['error_permission'] = 'Warning: You do not have permission to access the API! currency';
 $_['error_currency']   = 'Warning: Currency code is invalid!';
