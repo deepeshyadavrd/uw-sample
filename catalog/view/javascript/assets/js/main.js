79431d0f72dea1a9b0ba150d50976c77cf75df6c
deepeshyadavrd <raoshsbh6@gmail.com>
2025-06-06 15:33:29 +0530

    updated owl carousel

--- catalog/view/javascript/assets/js/main.js
+++ catalog/view/javascript/assets/js/main.js
@@ -45,6 +45,7 @@
             loop: true,
             autoplay: true,
             lazyLoad: true,
+            lazyLoadEager: 1,
             nav:navEnabled,
             navText: ['<i class="bx bx-chevron-left"></i>','<i class="bx bx-chevron-right"></i>'],
             dots: false, // This disables the dots
