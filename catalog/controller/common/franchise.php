0df2e59d711a8f1d0c40dcdc6b9c806d43d25654
deepeshyadavrd <raoshsbh6@gmail.com>
2025-08-05 17:46:01 +0530

    .

--- catalog/controller/common/franchise.php
+++ catalog/controller/common/franchise.php
@@ -4,21 +4,8 @@ class ControllerCommonFranchise extends Controller {
 private $error = [];
 	public function index() {
 
-		// $this->document->setTitle($this->config->get('config_meta_title'));
-
-		// $this->document->setDescription($this->config->get('config_meta_description'));
-
-		// $this->document->setKeywords($this->config->get('config_meta_keyword'));
-
-		// if (isset($this->request->get['route'])) {
-
-		// 	$this->document->addLink($this->config->get('config_url'), 'canonical');
-
-		// }
-
 		if (isset($this->session->data['success'])) {
 			$data['success'] = $this->session->data['success'];
-
 			unset($this->session->data['success']);
 		} else {
 			$data['success'] = '';
@@ -71,71 +58,42 @@ private $error = [];
             } else {
                 $result = $this->model_information_requestcallback->addFranchiseRequest($this->request->post);
 
-            // $to="deepeshurbanwood@gmail.com";
-            // $subject="Request Callback enquiry";
-            // $senderName = 'Urbanwood Furniture';
-            // $senderEmail = 'support@urbanwood.in';
+                $to="urbanwoodindia.in@gmail.com";
+                $subject="Request Callback enquiry";
+                $senderName = 'Urbanwood Furniture';
+                $senderEmail = 'support@urbanwood.in';
 
-            // $from = stripslashes($senderName)."<".stripslashes($senderEmail).">";
-            
-            // $mime_boundary="==Multipart_Boundary_x".md5(mt_rand())."x";
+                $from = stripslashes($senderName)."<".stripslashes($senderEmail).">";
 
-            // $headers = "From: $from\r\n" . 
-            // // "Cc: urbanwood.in@gmail.com\r\n".
-            // "MIME-Version: 1.0\r\n" .
-            // "Content-Type: multipart/mixed;\r\n" .
-            // " boundary=\"{$mime_boundary}\"";
-	        // if(isset($_POST["time_slot"]) || isset($_POST["date"])){
-	        // 	$message="Enquiry For Furniture (Request Callback)\n\n";
-	        // 	$message .= "Name:".$_POST["fullname"]."\n"."EMail:".$_POST['email']."\nMobile:".$_POST['mobile']."\nTime Slot:".$_POST["time_slot"]."\nDate:".$_POST["date"]."\nLooking For:".$_POST["message"];
-	        // }else{
-	        // 	$message="Enquiry For Furniture (Contact Form)\n\n";
-	        // 	$message .= "Name:".$_POST["fullname"]."\n"."EMail:".$_POST['email']."\nMobile:".$_POST['mobile']."\nLooking For:".$_POST["msg"];
-	        // }
-            // $message = "This is a multi-part message in MIME format.\n\n" .
-            // "--{$mime_boundary}\n" .
-            // "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
-            // "Content-Transfer-Encoding: 7bit\n\n" .
-            //  $message . "\n\n";
-        
-            // $i=0;
-            // $message.="--{$mime_boundary}--\n";
-            //  if (@mail($to, $subject, $message, $headers)){
-            // 	 $json['success'] = 'Service Working';//exit;
-            //  }else{
-            //     http_response_code(400);
-            //    $json['error'] =  'error -> '.error_get_last()['message'];//'Service Error';//exit;
-            //  }
-                   //$json['data'] = $this->request->post;
-                   $json['success'] = 'Form submitted successfully.';
+                $mime_boundary="==Multipart_Boundary_x".md5(mt_rand())."x";
+
+                $headers = "From: $from\r\n" . 
+                "Cc: urbanwood.in@gmail.com"."\r\n".
+                "MIME-Version: 1.0\r\n" .
+                "Content-Type: multipart/mixed;\r\n" .
+                "boundary=\"{$mime_boundary}\"";
+                	
+                		$message="Call Back Request\n\n";
+                		$message .= "Name: ".$_POST["fullname"]."\n"."EMail: ".$_POST['email']."\nMobile: ".$_POST['mobile']."\nCity: ".$_POST['city']."\nAlready own Franchise: ".$_POST["franchisee"]."\Have retail space: ".$_POST["retail_space"]."\Amount: ".$_POST["investamt"]."\nMessage: ".$_POST["msg"];
+
+                  $message = "This is a multi-part message in MIME format.\n\n" .
+                  "--{$mime_boundary}\n" .
+                  "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
+                  "Content-Transfer-Encoding: 7bit\n\n" .
+                   $message . "\n\n";
+                
+                 $i=0;
+                $message.="--{$mime_boundary}--\n";
+            if (@mail($to, $subject, $message, $headers)){
+            	 $json['success'] = 'Service Working';//exit;
+             }else{
+                $json['error'] = 'error -> '.error_get_last()['message'];//'Service Error';//exit;
+             }
         }
     }else{
         http_response_code(400);
          $json['errors'] = $this->error;
-    //    if (isset($this->error['name'])) {
-    //        $json['errors'] = $this->error;
-    //    } else {
-    //        $json['error_name'] = '';
-    //    }
-
-    //    if (isset($this->error['email'])) {
-    //        $json['errors'] = $this->error;
-    //    } else {
-    //        $json['error_email'] = '';
-    //    }
-
-    //    if (isset($this->error['mobile'])) {
-    //        $json['errors'] = $this->error;
-    //    } else {
-    //        $json['error_mobile'] = '';
-    //    }
-    //    if (isset($this->error['message'])) {
-    //        $json['errors'] = $this->error;
-    //    } else {
-    //        $json['error_message'] = '';
-    //    }
     }
-    // print_r($this->request->post['name']);
         $this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
     }
