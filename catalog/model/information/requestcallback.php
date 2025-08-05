de5d89b2e94c7c5885b3cf6f325a801a24c4717d
deepeshyadavrd <raoshsbh6@gmail.com>
2025-08-05 13:38:22 +0530

    .

--- catalog/model/information/requestcallback.php
+++ catalog/model/information/requestcallback.php
@@ -8,7 +8,8 @@ class ModelInformationRequestcallback extends Model {
 		return $rc_id;
 	}
 	public function addFranchiseRequest($data) {
-		$this->db->query("INSERT INTO " . DB_PREFIX . "franchise_request SET name = '" . $this->db->escape($data['fullname']) . "', email = '" . $this->db->escape($data['email']) . "', mobile = '" . $this->db->escape($data['mobile']) . "', city = '" . $this->db->escape($data['city']) . "', franchisee = '" . $this->db->escape($data['franchisee']) . "', retail_space = '" . $this->db->escape($data['retail_space']) . "', amount = '" . $this->db->escape($data['investamt']) . "', message = '" . $this->db->escape($data['msg']) . "', date_added = NOW(), status = 1");
+		// print_r($data);
+		$this->db->query("INSERT INTO " . DB_PREFIX . "franchise_request SET name = '" . $this->db->escape($data['fullname']) . "', email = '" . $this->db->escape($data['email']) . "', mobile = '" . $this->db->escape($data['mobile']) . "', city = '" . $this->db->escape($data['city']) . "', franchisee = '" . $this->db->escape($data['franchisee']) . "', retail_space = '" . $this->db->escape($data['retail_space']) . "', amount = '" . $this->db->escape($data['investamt']) . "', msg = '" . $this->db->escape($data['msg']) . "', date_added = NOW(), status = 1");
 
 		$rc_id = $this->db->getLastId();
 		
