0a95e337866a63550de7ed11409c9b6b5b098b9e
deepeshyadavrd <raoshsbh6@gmail.com>
2025-10-07 17:57:18 +0530

    added webp resize but not working yet

--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -13,7 +13,7 @@ class ModelToolImage extends Model {
 		if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
 			list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
 				 
-			if (!in_array($image_type, array(IMAGETYPE_AVIF,IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF))) { 
+			if (!in_array($image_type, array(IMAGETYPE_AVIF,IMAGETYPE_PNG, IMAGETYPE_JPEG, IMAGETYPE_GIF,IMAGETYPE_WEBP))) { 
 				return DIR_IMAGE . $image_old;
 			}
 						
@@ -32,33 +32,17 @@ class ModelToolImage extends Model {
 			if ($width_orig != $width || $height_orig != $height) {
 				$image = new Image(DIR_IMAGE . $image_old);
 				$image->resize($width, $height);
-				$image->save(DIR_IMAGE . $image_new);
+				// $image->save(DIR_IMAGE . $image_new);
+				if ($extension == 'webp') {
+            $image->save(DIR_IMAGE . $image_new, 90, 'webp');
+        } else {
+            $image->save(DIR_IMAGE . $image_new);
+        }
 			} else {
 				copy(DIR_IMAGE . $image_old, DIR_IMAGE . $image_new);
 			}
 		}
-		if (isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) {
-        $webp_cache_dir = str_replace('cache/', 'cachewebp/', dirname($new_image));
-        
-        if (!is_dir($webp_cache_dir)) {
-            mkdir($webp_cache_dir, 0777, true);
-        }
-        
-        $webp_image = str_replace('cache/', 'cachewebp/', $new_image);
-        $webp_image = preg_replace('/\.(jpg|jpeg|png|gif)$/i', '.webp', $webp_image);
-        
-        if (!is_file(DIR_IMAGE . $webp_image)) {
-            $image = new Image(DIR_IMAGE . $filename);
-            $image->resize($width, $height);
-            $image->save_webp(DIR_IMAGE . $webp_image, 90);
-        }
-        
-		if ($this->request->server['HTTPS']) {
-			return $this->config->get('config_ssl') . 'image/' . $webp_image;
-		} else {
-			return $this->config->get('config_url') . 'image/' . $webp_image;
-		}
-    }
+		
 		
 		$image_new = str_replace(' ', '%20', $image_new);  // fix bug when attach image on email (gmail.com). it is automatic changing space " " to +
 		
