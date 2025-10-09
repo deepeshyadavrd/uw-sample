fb4582008b3f7ed4a973c8ee15e74077b41a2d97
deepeshyadavrd <raoshsbh6@gmail.com>
2025-10-09 17:36:02 +0530

    made changes for webp image upload and display in filemanager

--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -9,7 +9,7 @@ class ModelToolImage extends Model {
 
 		$image_old = $filename;
 		$image_new = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . (int)$width . 'x' . (int)$height . '.' . $extension;
-print_r($image_new);
+// print_r($image_new);
 		if (!is_file(DIR_IMAGE . $image_new) || (filemtime(DIR_IMAGE . $image_old) > filemtime(DIR_IMAGE . $image_new))) {
 			list($width_orig, $height_orig, $image_type) = getimagesize(DIR_IMAGE . $image_old);
 				 
@@ -37,6 +37,25 @@ print_r($image_new);
 				copy(DIR_IMAGE . $image_old, DIR_IMAGE . $image_new);
 			}
 		}
+		// Create WebP version if browser supports it
+    if (isset($_SERVER['HTTP_ACCEPT']) && strpos($_SERVER['HTTP_ACCEPT'], 'image/webp') !== false) {
+        $webp_cache_dir = str_replace('cache/', 'cachewebp/', dirname($new_image));
+        
+        if (!is_dir($webp_cache_dir)) {
+            mkdir($webp_cache_dir, 0777, true);
+        }
+        
+        $webp_image = str_replace('cache/', 'cachewebp/', $new_image);
+        $webp_image = preg_replace('/\.(jpg|jpeg|png|gif)$/i', '.webp', $webp_image);
+        
+        if (!is_file(DIR_IMAGE . $webp_image)) {
+            $image = new Image(DIR_IMAGE . $filename);
+            $image->resize($width, $height);
+            $image->save_webp(DIR_IMAGE . $webp_image, 90);
+        }
+        
+        return $webp_image;
+    }
 		
 		$image_new = str_replace(' ', '%20', $image_new);  // fix bug when attach image on email (gmail.com). it is automatic changing space " " to +
 		
