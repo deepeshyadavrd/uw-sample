a023d9223f67cec0392c6b46c4564116c94d572c
deepesh yadav <raoshsbh6@gmail.com>
2026-02-13 16:44:03 +0530

    removed unused code from new arrival controller

--- catalog/controller/product/new_products.php
+++ catalog/controller/product/new_products.php
@@ -11,67 +11,62 @@ class ControllerProductNewProducts extends Controller {
 			$data['cid'] = '';
 		}
 
-		if (isset($this->request->get['filter'])) {
-			$filter = $this->request->get['filter'];
-		} else {
-			$filter = '';
-		}
+		// if (isset($this->request->get['filter'])) {
+		// 	$filter = $this->request->get['filter'];
+		// } else {
+		// 	$filter = '';
+		// }
 
-		if (isset($this->request->get['pr'])) {
-			$pr = $this->request->get['pr'];
-		} else {
-			$pr = '';
-		}
+		// if (isset($this->request->get['pr'])) {
+		// 	$pr = $this->request->get['pr'];
+		// } else {
+		// 	$pr = '';
+		// }
 
-		if (isset($this->request->get['sort'])) {
-			$sort = $this->request->get['sort'];
-		} else {
-			$sort = 'p.sort_order';
-		}
+		// if (isset($this->request->get['sort'])) {
+		// 	$sort = $this->request->get['sort'];
+		// } else {
+		// 	$sort = 'p.sort_order';
+		// }
 
-		if (isset($this->request->get['order'])) {
-			$order = $this->request->get['order'];
-		} else {
-			$order = 'ASC';
-		}
+		// if (isset($this->request->get['order'])) {
+		// 	$order = $this->request->get['order'];
+		// } else {
+		// 	$order = 'ASC';
+		// }
 
-		if (isset($this->request->get['page'])) {
-			$page = $this->request->get['page'];
-		} else {
-			$page = 1;
-		}
+		// if (isset($this->request->get['page'])) {
+		// 	$page = $this->request->get['page'];
+		// } else {
+		// 	$page = 1;
+		// }
 
-		if (isset($this->request->get['limit'])) {
-			$limit = (int)$this->request->get['limit'];
-		} else {
-			$limit = $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit');
-		}
+		// if (isset($this->request->get['limit'])) {
+		// 	$limit = (int)$this->request->get['limit'];
+		// } else {
+		// 	$limit = $this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit');
+		// }
         $data['products'] = [];
-        $url = '';
+        // $url = '';
 
-        if (isset($this->request->get['filter'])) {
-            $url .= '&filter=' . $this->request->get['filter'];
-        }
+        // if (isset($this->request->get['filter'])) {
+        //     $url .= '&filter=' . $this->request->get['filter'];
+        // }
 
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
+        // if (isset($this->request->get['sort'])) {
+        //     $url .= '&sort=' . $this->request->get['sort'];
+        // }
 
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
+        // if (isset($this->request->get['order'])) {
+        //     $url .= '&order=' . $this->request->get['order'];
+        // }
 
-        if (isset($this->request->get['limit'])) {
-            $url .= '&limit=' . $this->request->get['limit'];
-        }
-        $limit = 50;
+        // if (isset($this->request->get['limit'])) {
+        //     $url .= '&limit=' . $this->request->get['limit'];
+        // }
+        // $limit = 50;
         
-        $results = $this->model_catalog_product->getProductsNewArrival([
-            'sort' => 'p.date_added',
-            'order' => 'DESC',
-            'start' => 0,
-            'limit' => $limit
-        ]);
+        $results = $this->model_catalog_product->getProductsNewArrival();
 // print_r($results); 
         foreach ($results as $result) {
             if ($result['image']) {
@@ -115,6 +110,7 @@ class ControllerProductNewProducts extends Controller {
 					'thumb'       	=> $image,
 					'name'        	=> $name,
 					'finish'        => $finish,
+                    'special'     	=> $result['price'],
 					// 'description' 	=> utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
 					// 'price'       	=> $price,
 					// 'special'     	=> $special,
@@ -130,60 +126,60 @@ class ControllerProductNewProducts extends Controller {
 					'pro_gro' => $pro_gro
 				);
         }
-        $url = '';
+        // $url = '';
 
-        if (isset($this->request->get['filter'])) {
-            $url .= '&filter=' . $this->request->get['filter'];
-        }
-        if (isset($this->request->get['limit'])) {
-            $url .= '&limit=' . $this->request->get['limit'];
-        }
-        $url = '';
+        // if (isset($this->request->get['filter'])) {
+        //     $url .= '&filter=' . $this->request->get['filter'];
+        // }
+        // if (isset($this->request->get['limit'])) {
+        //     $url .= '&limit=' . $this->request->get['limit'];
+        // }
+        // $url = '';
 
-        if (isset($this->request->get['filter'])) {
-            $url .= '&filter=' . $this->request->get['filter'];
-        }
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-        $data['limits'] = array();
-        $limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
-        sort($limits);
-        foreach($limits as $value) {
-            $data['limits'][] = array(
-                'text'  => $value,
-                'value' => $value,
-                'href'  => $this->url->link('product/category', 'path=' . $this->request->get['path'] . $url . '&limit=' . $value, true)
-            );
-        }
-        $url = '';
-        if (isset($this->request->get['filter'])) {
-            $url .= '&filter=' . $this->request->get['filter'];
-        }
-        if (isset($this->request->get['sort'])) {
-            $url .= '&sort=' . $this->request->get['sort'];
-        }
-        if (isset($this->request->get['order'])) {
-            $url .= '&order=' . $this->request->get['order'];
-        }
-        if (isset($this->request->get['limit'])) {
-            $url .= '&limit=' . $this->request->get['limit'];
-        }
+        // if (isset($this->request->get['filter'])) {
+        //     $url .= '&filter=' . $this->request->get['filter'];
+        // }
+        // if (isset($this->request->get['sort'])) {
+        //     $url .= '&sort=' . $this->request->get['sort'];
+        // }
+        // if (isset($this->request->get['order'])) {
+        //     $url .= '&order=' . $this->request->get['order'];
+        // }
+        // $data['limits'] = array();
+        // $limits = array_unique(array($this->config->get('theme_' . $this->config->get('config_theme') . '_product_limit'), 25, 50, 75, 100));
+        // sort($limits);
+        // foreach($limits as $value) {
+        //     $data['limits'][] = array(
+        //         'text'  => $value,
+        //         'value' => $value,
+        //         'href'  => $this->url->link('product/category', 'path=' . $this->request->get['path'] . $url . '&limit=' . $value, true)
+        //     );
+        // }
+        // $url = '';
+        // if (isset($this->request->get['filter'])) {
+        //     $url .= '&filter=' . $this->request->get['filter'];
+        // }
+        // if (isset($this->request->get['sort'])) {
+        //     $url .= '&sort=' . $this->request->get['sort'];
+        // }
+        // if (isset($this->request->get['order'])) {
+        //     $url .= '&order=' . $this->request->get['order'];
+        // }
+        // if (isset($this->request->get['limit'])) {
+        //     $url .= '&limit=' . $this->request->get['limit'];
+        // }
         
-        $data['sort'] = $sort;
-        $data['order'] = $order;
-        $data['limit'] = $limit;
-        $data['pr'] = $pr;
-        $data['filters_data'] = $filter;
+        // $data['sort'] = $sort;
+        // $data['order'] = $order;
+        // $data['limit'] = $limit;
+        // $data['pr'] = $pr;
+        // $data['filters_data'] = $filter;
         $data['heading_title'] = 'New Arrivals';
 
         $data['footer'] = $this->load->controller('common/footer');
 			$data['header'] = $this->load->controller('common/header');
 			$data['menu'] = $this->load->controller('common/menu');
-			$data['filter'] = $this->load->controller('extension/module/filter');
+			// $data['filter'] = $this->load->controller('extension/module/filter');
         $this->response->setOutput($this->load->view('product/new_products', $data));
     }
 }
