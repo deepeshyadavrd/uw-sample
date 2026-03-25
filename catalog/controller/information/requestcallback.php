1b6f2ff5c12d35f13b1096eebc40e719d325510b
deepesh yadav <raoshsbh6@gmail.com>
2026-03-25 18:57:04 +0530

    landing page updated for ggn, tracking cart and checkoy and purchase

--- catalog/controller/information/requestcallback.php
+++ catalog/controller/information/requestcallback.php
@@ -50,4 +50,49 @@ class ControllerInformationRequestcallback extends Controller {
     $this->response->addHeader('Content-Type: application/json');
 		$this->response->setOutput(json_encode($json));
   }
+
+  public function newForm() {
+    $this->load->model('information/requestcallback');
+    if($_POST['name'] != ' ' && $_POST['name'] != NULL && $_POST['mobile'] != ' ' && $_POST['mobile'] != NULL){
+      $result = $this->model_information_requestcallback->addRequestcallback($this->request->post);
+      $json = array();
+      $to="urbanwoodindia.in@gmail.com";
+      $subject="Request Callback enquiry";
+      $senderName = 'Urbanwood Furniture';
+      $senderEmail = 'support@urbanwood.in';
+
+      $from = stripslashes($senderName)."<".stripslashes($senderEmail).">";
+
+      $mime_boundary="==Multipart_Boundary_x".md5(mt_rand())."x";
+
+      $headers = "From: $from\r\n" . 
+      "Cc: urbanwood.in@gmail.com"."\r\n".
+      "MIME-Version: 1.0\r\n" .
+      "Content-Type: multipart/mixed;\r\n" .
+      " boundary=\"{$mime_boundary}\"";
+
+      $message="New Form Request\n\n";
+      $message .= "Name: ".$_POST["name"]."\nMobile: ".$_POST['mobile']."\nLooking For: ".$_POST["message"];
+
+      $message = "This is a multi-part message in MIME format.\n\n" .
+      "--{$mime_boundary}\n" .
+      "Content-Type: text/plain; charset=\"iso-8859-1\"\n" .
+      "Content-Transfer-Encoding: 7bit\n\n" .
+       $message . "\n\n";
+
+      $i=0;
+      $message.="--{$mime_boundary}--\n";
+      if (@mail($to, $subject, $message, $headers)){
+        $json['success'] = 'Service Working';//exit;
+      }else{
+       $json['error'] = 'error -> '.error_get_last()['message'];//'Service Error';//exit;
+      }
+          //$json['data'] = $this->request->post;
+          $this->response->addHeader('Content-Type: application/json');
+        $this->response->setOutput(json_encode($json));
+      }
+      $json['error'] = 'empty data';
+      $this->response->addHeader('Content-Type: application/json');
+      $this->response->setOutput(json_encode($json));
+    }
 }
