d7a9385d40759489c907e665a417334952814340
deepesh yadav <raoshsbh6@gmail.com>
2026-07-16 16:49:41 +0530

    generated category, product, pages and index sitemap, updated controller to generate all at once and separately also

--- catalog/controller/extension/feed/sitemaps.php
+++ catalog/controller/extension/feed/sitemaps.php
@@ -1,68 +1,164 @@
 <?php
 class ControllerExtensionFeedSitemaps extends Controller {
     public function index() {
-        if ($this->config->get('feed_google_sitemap_status')) {
-            $output = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
-            $output .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
-            
-            $baseUrl = $this->config->get('config_url');
 
-            // Product Sitemap
-            $output .= "    <sitemap>\n";
-            $output .= "        <loc>{$baseUrl}sitemap-products.xml</loc>\n";
-            $output .= "        <lastmod>" . date('Y-m-d') . "</lastmod>\n";
-            $output .= "    </sitemap>\n";
-
-            // Category Sitemap
-            $output .= "    <sitemap>\n";
-            $output .= "        <loc>{$baseUrl}sitemap-categories.xml</loc>\n";
-            $output .= "        <lastmod>" . date('Y-m-d') . "</lastmod>\n";
-            $output .= "    </sitemap>\n";
-
-            // Information Pages Sitemap
-            $output .= "    <sitemap>\n";
-            $output .= "        <loc>{$baseUrl}sitemap-info.xml</loc>\n";
-            $output .= "        <lastmod>" . date('Y-m-d') . "</lastmod>\n";
-            $output .= "    </sitemap>\n";
-
-            $output .= '</sitemapindex>';
-
-            $this->response->addHeader('Content-Type: application/xml');
-            $this->response->setOutput($output);
-        }
-    }
-    public function generateSitemapIndex() {
-
-        $base = HTTPS_SERVER;
+        $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
     
-        $xml  = '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
-        $xml .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">' . "\n";
+        $xml .= '<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
     
         $xml .= '<sitemap>';
-        $xml .= '<loc>' . $base . 'sitemap-products.xml</loc>';
+        $xml .= '<loc>' . HTTPS_SERVER . 'sitemap-products.xml</loc>';
         $xml .= '</sitemap>';
     
         $xml .= '<sitemap>';
-        $xml .= '<loc>' . $base . 'sitemap-categories.xml</loc>';
+        $xml .= '<loc>' . HTTPS_SERVER . 'sitemap-categories.xml</loc>';
         $xml .= '</sitemap>';
     
         $xml .= '<sitemap>';
-        $xml .= '<loc>' . $base . 'sitemap-pages.xml</loc>';
+        $xml .= '<loc>' . HTTPS_SERVER . 'sitemap-pages.xml</loc>';
         $xml .= '</sitemap>';
     
         $xml .= '</sitemapindex>';
     
         file_put_contents(DIR_APPLICATION . '../sitemap-base.xml', $xml);
+    
+        $this->response->addHeader('Content-Type: application/xml');
+        $this->response->setOutput($xml);
+    }
+    public function products() {
+        $this->load->model('extension/feed/sitemaps');
+        $products = $this->model_extension_feed_sitemaps->getProducts();
+    
+        $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    
+        foreach ($products as $product) {
+    
+            $xml .= '<url>';
+    
+            $xml .= '<loc>' .
+                $this->url->link(
+                    'product/product',
+                    'product_id=' . $product['product_id'],
+                    true
+                ) .
+                '</loc>';
+    
+            if (!empty($product['date_modified'])) {
+                $xml .= '<lastmod>' .
+                    date('Y-m-d', strtotime($product['date_modified'])) .
+                    '</lastmod>';
+            }
+    
+            $xml .= '<changefreq>weekly</changefreq>';
+            $xml .= '<priority>0.8</priority>';
+    
+            $xml .= '</url>';
+        }
+    
+        $xml .= '</urlset>';
+    
+        file_put_contents(DIR_APPLICATION . '../sitemap-products.xml', $xml);
+    
+        $this->response->addHeader('Content-Type: application/xml');
+        $this->response->setOutput($xml);
+    }
+
+    public function categories() {
+        $this->load->model('extension/feed/sitemaps');
+        $categories = $this->model_extension_feed_sitemaps->getCategories();
+    
+        $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    
+        foreach ($categories as $category) {
+    
+            $xml .= '<url>';
+    
+            $xml .= '<loc>' .
+                $this->url->link(
+                    'product/category',
+                    'path=' . $category['category_id'],
+                    true
+                ) .
+                '</loc>';
+    
+            $xml .= '<changefreq>weekly</changefreq>';
+            $xml .= '<priority>0.7</priority>';
+    
+            $xml .= '</url>';
+        }
+    
+        $xml .= '</urlset>';
+    
+        file_put_contents(DIR_APPLICATION . '../sitemap-categories.xml', $xml);
+    
+        $this->response->addHeader('Content-Type: application/xml');
+        $this->response->setOutput($xml);
+    }
+
+    public function pages() {
+        $this->load->model('extension/feed/sitemaps');
+        $urls = [];
+
+        // Homepage
+        // $urls[] = HTTPS_SERVER;
+
+        // Static pages
+        // $urls[] = HTTPS_SERVER . 'contact-us';
+        // $urls[] = HTTPS_SERVER . 'postal-address';
+        $informations = $this->model_extension_feed_sitemaps->getInformations();
+        // foreach ($informations as $info) {
+
+        //     $urls[] = $this->url->link(
+        //         'information/information',
+        //         'information_id=' . $info['information_id'],
+        //         true
+        //     );
+        // }
+    
+        $xml  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+    
+        $xml .= '<url>';
+        $xml .= '<loc>' . HTTPS_SERVER . '</loc>';
+        $xml .= '<changefreq>daily</changefreq>';
+        $xml .= '<priority>1.0</priority>';
+        $xml .= '</url>';
+    
+        foreach ($informations as $info) {
+    
+            $xml .= '<url>';
+    
+            $xml .= '<loc>' .
+                $this->url->link(
+                    'information/information',
+                    'information_id=' . $info['information_id'],
+                    true
+                ) .
+                '</loc>';
+    
+            $xml .= '<changefreq>monthly</changefreq>';
+            $xml .= '<priority>0.6</priority>';
+    
+            $xml .= '</url>';
+        }
+    
+        $xml .= '</urlset>';
+    
+        file_put_contents(DIR_APPLICATION . '../sitemap-pages.xml', $xml);
+    
+        $this->response->addHeader('Content-Type: application/xml');
+        $this->response->setOutput($xml);
     }
 
     public function generate() {
-        $this->load->model('extension/feed/sitemaps');
 
-        // Generate different sitemap sections
-        $this->model_extension_feed_sitemaps->generateProductsSitemap();
-        $this->model_extension_feed_sitemaps->generateCategoriesSitemap();
-        $this->model_extension_feed_sitemaps->generateInfoPagesSitemap();
-
-        echo "✅ Sitemaps Generated Successfully!";
+        $this->products();
+        $this->categories();
+        $this->pages();
+        $this->index();
+    
+        echo 'Sitemaps Generated Successfully';
     }
 }
